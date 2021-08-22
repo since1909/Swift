cd4bdb9c2e403f9b6e0b29db22b904f9f9850709
since1909 <since1909@naver.com>
2021-08-22 16:30:52 +0900

    편집 취소 alert 추가

--- myMemo/myMemo/ComposeViewController.swift
+++ myMemo/myMemo/ComposeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ComposeViewController: UIViewController {
     
     var editTarget: Memo?
+    var originalMemoContent: String? //편집 이전의 메모 내용
     
     @IBOutlet var memoTextView: UITextView!
     
@@ -35,8 +36,6 @@ class ComposeViewController: UIViewController {
             NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
         }
         
-        //boradcasting 방식
-        
         dismiss(animated: true, completion: nil)
     }
     
@@ -46,14 +45,26 @@ class ComposeViewController: UIViewController {
         if let memo = editTarget {
             navigationItem.title = "메모 편집"
             memoTextView.text = memo.content
-            NotificationCenter.default.post(name: ComposeViewController.memoDidChange, object: nil)
+            originalMemoContent = memo.content
         } else {
             navigationItem.title = "새 메모"
             memoTextView.text = ""
         }
-        // Do any additional setup after loading the view.
+        
+        memoTextView.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.presentationController?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.presentationController?.delegate = nil
+    }
 
     /*
     // MARK: - Navigation
@@ -65,6 +76,38 @@ class ComposeViewController: UIViewController {
     }
     */
 
+}
+
+extension ComposeViewController: UITextViewDelegate {
+    //text 편집시 자동으로 호출
+    func textViewDidChange(_ textView: UITextView) {
+        if let original = originalMemoContent, let edited = textView.text {
+            if #available(iOS 13.0, *) {
+                //편집됐다면 true 저장
+                isModalInPresentation = original != edited
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+}
+
+extension ComposeViewController: UIAdaptivePresentationControllerDelegate {
+    //isModalInPresentation 이 true이고 sheet를 밑으로 내리려고 할때 호출
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        let alert = UIAlertController(title: "알림", message: "편집한 내용이 저장되지 않았습니다.", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .default, handler: { [weak self] action in
+            self?.close(action)
+        })
+        alert.addAction(cancelAction)
+        
+        let saveAction = UIAlertAction(title: "저장", style: .default, handler: { [weak self] action in
+            self?.save(action)
+        })
+        alert.addAction(saveAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 extension ComposeViewController {
